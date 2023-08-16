511a12a0969df0781ba6ca0a66e1c1b49429700e
jnysevilla <116690031+jnysevilla@users.noreply.github.com>
2023-08-16 16:42:28 +0100

    Modified html + css + javascript

--- script.js
+++ script.js
@@ -39,3 +39,55 @@ function loadComponent(componentUrl, containerId) {
   xhr.open("GET", componentUrl, true);
   xhr.send();
 }
+
+// Subscribe Button 
+document.addEventListener("DOMContentLoaded", function() {
+  const popup = document.getElementById("subscription-popup");
+  const openPopupButton = document.getElementById("open-popup");
+  const closePopupButton = document.getElementById("close-popup");
+  const thankYouMessage = document.getElementById("thank-you-message");
+  const subscribeForm = document.getElementById("subscribe-form");
+  const emailInput = document.getElementById("email");
+  const emailErrorMessage = document.getElementById("email-error");
+  const invalidEmailMessage = document.getElementById("invalid-email-error");
+
+  openPopupButton.addEventListener("click", function() {
+    popup.style.display = "flex";
+  });
+
+  closePopupButton.addEventListener("click", function() {
+    popup.style.display = "none";
+    thankYouMessage.style.display = "none"; // Hide thank you message on close
+  });
+
+  subscribeForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const emailValue = emailInput.value.trim();
+
+    if (emailValue === "") {
+      emailErrorMessage.style.display = "block";
+      invalidEmailMessage.style.display = "none";
+    } else if (!isValidEmail(emailValue)) {
+      emailErrorMessage.style.display = "none";
+      invalidEmailMessage.style.display = "block";
+    } else {
+      emailErrorMessage.style.display = "none";
+      invalidEmailMessage.style.display = "none";
+      // Simulate successful subscription for demonstration
+      // In real use, you would likely submit form data to a server
+      thankYouMessage.style.display = "block"; // Display thank you message
+      emailInput.value = ""; // Clear the input field
+    }
+  });
+
+  emailInput.addEventListener("input", function() {
+    emailErrorMessage.style.display = "none";
+    invalidEmailMessage.style.display = "none";
+  });
+
+  function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+});
