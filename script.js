2e4f3c4f05be9f312e0e2d27a4fc41adf93a8ac3
jnysevilla <116690031+jnysevilla@users.noreply.github.com>
2023-08-19 20:12:16 +0100

    Modified index.html and script.js

--- script.js
+++ script.js
@@ -113,7 +113,6 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 });
 
-
 // Modal
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
